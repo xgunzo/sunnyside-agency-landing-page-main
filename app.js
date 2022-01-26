69ae200106f220bf0debb6a4d0b950734be93688
xgunzo <mchmiel07@gmail.com>
2022-01-26 01:20:34 +0100

    final version 1.0

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 window.onload = function() {
     let menuButton = document.getElementById("menu-button");
     let active = document.getElementById("active");
+    let arrow = document.getElementById("arrow");
     let body = document.body;
-    
     
     menuButton.addEventListener("click", function(e) {
         active.style.display = "block";
+        arrow.style.display = "none";
         e.stopPropagation();
 
     body.addEventListener("click", function(){
             active.style.display = "none";
+            arrow.style.display = "block";
         } )
 
         active.addEventListener("click", function(e) {
+            arrow.style.display = "absolute";
             e.stopPropagation();
         })
 
